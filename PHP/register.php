e30d0837a305a334a50e5b5e21a8509deb391e06
Muhamad Darul Hadi bin Zainal Abidin <47632993+muhamaddarulhadi@users.noreply.github.com>
2020-02-03 18:33:20 +0800

    update register.php

--- PHP/register.php
+++ PHP/register.php
@@ -1,7 +1,7 @@
 <?php
 
-$staffid = $_POST['StaffID'];
-$fullname = $_POST['Fullname'];
+$staffid = $_POST['StaffID'];    //StaffID ni dia amik dari name="StaffID"
+$fullname = $_POST['Fullname'];   //semua name yang dalam array
 $usernameR = $_POST['Username'];
 $passwordR = $_POST['Password'];
 $icNumber = $_POST['IC_Number'];
@@ -12,10 +12,11 @@ $position = $_POST['position'];
 
 if (!empty($staffid) || !empty($fullname) || !empty($usernameR) || !empty($passwordR) || !empty($icNumber) || !empty($phoneNumber) || !empty($email) || !empty($position)) 
 {
+    //dalam phpmyadmin tu, kita set 
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $db = "HRCS";
+    $db = "HRCS";  //nama database dlm phpmyadmin
 
     // Create connection
     $con = mysqli_connect($servername, $username, $password,$db);
@@ -47,9 +48,9 @@ if (!empty($staffid) || !empty($fullname) || !empty($usernameR) || !empty($passw
                 $stmt->close();
                 //$stmt = $con->prepare("INSERT INTO doctor_register(STAFF_ID, USERNAME, PASSWORDS, POSITION, IC_NUM, EMAIL, FULLNAME, PHONE_NUM) values($staffid, $usernameR, $passwordR, $position, $icNumber, $email, $fullname, $phoneNumber)");
                 $stmt = $con->prepare($INSERT);
-                $stmt->bind_param("ssssssss", $staffid, $usernameR, $passwordR, $position, $icNumber,  $email, $fullname, $phoneNumber); 
+                $stmt->bind_param("ssssssss", $staffid, $usernameR, $passwordR, $position, $icNumber,  $email, $fullname, $phoneNumber); // yg ssss banyak2 tu sebab s tu menandakan value yang store dlm database adalah string
                 $stmt->execute();
-                echo "Success to register";
+                echo "Success to register"; // yang ni nak test ja, kalau taknak dia muncul another page yg tunjuk success ni, buat success pop out guns javascript, boleh cari dalam online
             } 
             else 
             {
@@ -57,6 +58,11 @@ if (!empty($staffid) || !empty($fullname) || !empty($usernameR) || !empty($passw
             }
             $stmt->close();
             $con->close();
+        }
+        
+        if ($position=="Nurse")
+        {
+            //buat sebijik macam doctor yang kat atas tu, amik semua dalam if doctor dan ubah nama table nurse_register dan column sahaja
         }
     }
 } 
